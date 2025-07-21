55383b6be64d5134509eb4de863c598a9c49f84a
dhruv patel <dhruvjpatel5@gmail.com>
2025-07-21 11:43:14 +0530

    Update MongoDB URI in .env, add router dependency, and implement rooms route in server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 const dbConfig = require('./db')
+const roomsRoute = require("./routes/roomsRoute")
+
+app.use('/api/rooms',roomsRoute)
 
 app.listen(PORT, () => {
     console.log(`Server is running on: http://localhost:${PORT}`);
