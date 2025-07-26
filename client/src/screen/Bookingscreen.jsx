d71a50e945939595e7c607caca40b518821fc184
dhruv patel <dhruvjpatel5@gmail.com>
2025-07-26 08:22:37 +0530

    Fix typo in Bookingscreen component comment

--- client/src/screen/Bookingscreen.jsx
+++ client/src/screen/Bookingscreen.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-
+//dewre
 const Bookingscreen = () => {
   const { roomid } = useParams();
   const [room, setRoom] = useState();
